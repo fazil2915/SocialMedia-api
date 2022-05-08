7b88d59680f3f6a62cc90606c0718cb2143a86e3
fazil rahman <fazil2915@gmail.com>
2022-05-08 13:50:23 +0530

    user-update

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,32 @@
+const User=require("../models/User");
 const router=require("express").Router();
+const bcrypt=require("bcrypt");     
 
-router.get("/",(req,res)=>{
-    res.send("welcome user")
+//update user 
+//delete a user
+//follow a user
+//unfollow a user
+
+router.put("/:id",async(req,res)=>{
+if(req.body.userId===req.params.id||req.user.isAdmin){
+    if(req.body.password){
+        try{
+            const salt=await bcrypt.genSalt(10);
+            req.body.password=await bcrypt.hash(req.body.password,salt);
+        }
+        catch(err){
+            return res.status(500).json(err);
+        }
+    }try{
+        const user=await User.findByIdAndUpdate(req.params.id,{
+            $set:req.body,
+        });
+        res.status(200).json("Account has been updated")
+    }catch(err){
+        return res.status(500).json(err);
+    }
+}else{
+    return res.status(403).json("The account not yours");
+}
 })
-
 module.exports=router
